282179bcc8edaa5f76c6922636b3a3f1cbc47be5
radamin <radamin@yandex.ru>
2023-04-24 00:14:52 +0300

    Add Task_1

--- src/main/java/Homework_2/Main.java
+++ src/main/java/Homework_2/Main.java
@@ -1,4 +1,31 @@
 package Homework_2;
 
+import java.util.Arrays;
+
+
 public class Main {
+    public static void main(String[] args) {
+        String stringToFilter = "\"name:Ivanov\" \"country:null\" \"city:null\" \"age:null\"";
+        String[] stringToFilterArrayNew = stringToFilter.split("\"?\\s+\"?|:\"?");
+        System.out.println(Arrays.toString(stringToFilterArrayNew));
+        //if (stringToFilterArrayNew.length > 0) { // проверяем, что массив не пустой
+        //String[] keyValuePairs = stringToFilterArrayNew[0].split(",");
+        StringBuilder filterBuilder = new StringBuilder();
+
+        for (int i = 0; i < stringToFilterArrayNew.length + 1; i += 2) {
+            if (i + 1 < stringToFilterArrayNew.length) { // проверяем, что есть следующий элемент
+                String key = stringToFilterArrayNew[i];
+                String value = stringToFilterArrayNew[i + 1];
+                if (!value.equals("null\"") && !value.equals("null")) {
+                    if (filterBuilder.length() > 0) {
+                        filterBuilder.append(" and ");
+                    }
+                    filterBuilder.append(key).append(" = '").append(value).append("'\"");
+                }
+            }
+        }
+
+        String filter = filterBuilder.toString();
+        System.out.println(filter);
+    }
 }
