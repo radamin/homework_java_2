79b167b63673584d3524007f4ee0c82f6bcb4b76
radamin <radamin@yandex.ru>
2023-04-24 00:26:38 +0300

    add code

--- src/main/java/Homework_2/Main.java
+++ src/main/java/Homework_2/Main.java
@@ -8,12 +8,10 @@ public class Main {
         String stringToFilter = "\"name:Ivanov\" \"country:null\" \"city:null\" \"age:null\"";
         String[] stringToFilterArrayNew = stringToFilter.split("\"?\\s+\"?|:\"?");
         System.out.println(Arrays.toString(stringToFilterArrayNew));
-        //if (stringToFilterArrayNew.length > 0) { // проверяем, что массив не пустой
-        //String[] keyValuePairs = stringToFilterArrayNew[0].split(",");
         StringBuilder filterBuilder = new StringBuilder();
 
         for (int i = 0; i < stringToFilterArrayNew.length + 1; i += 2) {
-            if (i + 1 < stringToFilterArrayNew.length) { // проверяем, что есть следующий элемент
+            if (i + 1 < stringToFilterArrayNew.length) { // следующий элемент
                 String key = stringToFilterArrayNew[i];
                 String value = stringToFilterArrayNew[i + 1];
                 if (!value.equals("null\"") && !value.equals("null")) {
